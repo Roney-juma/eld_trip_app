337959239c80d820110965481982f48f6e25c11d
Roney <roney@NDK36-roney-mac.local>
2025-03-21 16:27:43 +0300

    Trip crud operations

--- trip/views.py
+++ trip/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Trip
+from .serializers import TripSerializer
 
-# Create your views here.
+class TripListCreateView(generics.ListCreateAPIView):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
