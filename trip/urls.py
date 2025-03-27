6ed19a9e398681bca5f270a082a6102220ed78dd
Roney <roney@NDK36-roney-mac.local>
2025-03-27 12:22:33 +0300

    Routew for Downloading

--- trip/urls.py
+++ trip/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_trip, trip_history
+from .views import create_trip, trip_history, download_trip_log
 
 urlpatterns = [
     path('trips/', create_trip, name='create-trip'),
     path('history/', trip_history, name='trip-history'),
+    path('download/<int:trip_id>/', download_trip_log, name='download-trip-log'),
 ]
